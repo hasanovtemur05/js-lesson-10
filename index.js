5c6696284729641341c082db6b98c0f0030f9c1e
hasanovtemur05 <hasanovtemur86@gmail.com>
2024-07-09 11:58:30 +0500

    added about

---
+++ index.js
@@ -0,0 +1,69 @@
+//  1 - masala
+// let num = prompt("son kiriting:");
+// const sumNumbers = (num) => {
+//     return num.toString().split("").reduce((acc, son) => acc + son , 0);
+//   };
+//   let sum = sumNumbers(num);
+//   console.log(`Natija: ${sum}`);
+
+
+
+// 2 - masala
+// let num = [11,2,3];
+// const sumNumbers = (num) => {
+//     let result =num.reduce((sum, son) => sum + son, 0); 
+//     console.log(result);
+//   }
+//   let sum = sumNumbers(num);
+
+
+
+// 3 - masala
+
+// let num = prompt("son kiriting:");
+// const sumNumbers = (num) => {
+//     let result = num.toString().split("").reverse().join("");
+//     if (num === result) {
+//         console.log("palindrome");
+//     }else{
+//         console.log("palindrome emas");
+//     }
+//   };
+// sumNumbers(num);
+  
+
+// 4 - masala
+// const function2 = () =>{
+//     let a = +parseInt(Math.random()* 50)
+//     let b = +parseInt(Math.random()* 50)
+//     console.log("birinchi random son", a);
+//     console.log("ikkinchi random son", b);
+//    const function1 = () =>{
+//     let result = a + b
+//     console.log(result);
+//    }
+//    function1()
+// }
+// function2()
+
+
+
+// 5 - masala
+// let str = prompt("suz kiriting")
+// const strPalindrome = () =>{
+//     let result = str.toString().split("").reverse().join("");
+//     if (result === str) {
+//         console.log("palindrome");
+//     }else{
+//         console.log("palindrome emas");
+//     }
+// }
+// strPalindrome()
+
+
+
+
+// 6 - masala
+// let obj = {a:1,b:22} 
+// console.log(Object.values(obj).reduce((a, b) => a + b , 0));
+
